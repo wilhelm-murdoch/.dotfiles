1826dded43fd682e33f72e48924f88fba62431aa
Wilhelm Murdoch <wilhelm@devilmayco.de>
2025-02-03 23:43:59 +1000

    updating git gutter symbols

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -4,8 +4,8 @@ return { -- Adds git related signs to the gutter, as well as utilities for manag
 		signs = {
 			add = { text = "+" },
 			change = { text = "~" },
-			delete = { text = "_" },
-			topdelete = { text = "‾" },
+			delete = { text = "-" },
+			topdelete = { text = "-" },
 			changedelete = { text = "~" },
 		},
 	},
