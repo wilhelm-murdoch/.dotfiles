7f85d255806cc360913855efe8e8cd4e978053be
Wilhelm Default <default@local.host>
2025-10-29 01:32:36 +1000

    auto-accept blink.cmp selection on <tab>

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -5,7 +5,20 @@ return {
 	opts = {
 		signature = { enabled = true },
 
-		keymap = { preset = "default" },
+		keymap = {
+			preset = "default",
+			["<Tab>"] = {
+				function(cmp)
+					if cmp.snippet_active() then
+						return cmp.accept()
+					else
+						return cmp.select_and_accept()
+					end
+				end,
+				"snippet_forward",
+				"fallback",
+			},
+		},
 
 		appearance = {
 			nerd_font_variant = "mono",
