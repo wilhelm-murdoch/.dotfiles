b6d7e3385216ac77586f13ca63baf1738cdf6c31
Wilhelm Default <default@local.host>
2026-06-07 21:38:48 +1000

    configure twilight

--- nvim/lua/plugins/twilight.lua
+++ nvim/lua/plugins/twilight.lua
@@ -1,18 +1,18 @@
 return {
-	"folke/twilight.nvim",
-	event = "VeryLazy",
-	opts = {
-		context = 10,
-		dimming = {
-			alpha = 0.25,
-		},
-		expand = {
-			"function",
-			"method",
-		},
-	},
-	config = function(_, opts)
-		require("twilight").setup(opts)
-		require("twilight").enable()
-	end,
+	-- "folke/twilight.nvim",
+	-- event = "VeryLazy",
+	-- opts = {
+	-- 	context = 10,
+	-- 	dimming = {
+	-- 		alpha = 0.25,
+	-- 	},
+	-- 	expand = {
+	-- 		"function",
+	-- 		"method",
+	-- 	},
+	-- },
+	-- config = function(_, opts)
+	-- 	require("twilight").setup(opts)
+	-- 	require("twilight").enable()
+	-- end,
 }
